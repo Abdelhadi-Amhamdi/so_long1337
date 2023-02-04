0283b5d0637f0dd239bfc96647c546f512345c53
Abdelhadi Amhamdi <aamhamdi@e1r8p13.1337.ma>
2023-02-04 15:28:00 +0100

    [fix] draw map function

--- draw_map.c
+++ draw_map.c
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:16:46 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/27 16:29:06 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:14:52 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,44 @@
 #include "libft/libft.h"
 #include <mlx.h>
 
-void ft_draw_map(to_long *game, t_images *images, t_list *map)
+void ft_draw_map(to_long *game, t_images *images)
 {
-    int i = 0;
     int x = 0;
     int y = 0;
-    
-    while(map)
+    char **map;
+
+    map = game->map;
+    game->collects_n = 0;
+    while (map[y])
     {
-        i= 0;
         x = 0;
-        char *tab = (char *)map->content;
-        while(tab[i])
+        while (map[y][x])
         {
-            if(tab[i] == '1')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->wall_image, x * 50, y * 50);
-            else if(tab[i] == 'P')
+            if (map[y][x] == '1')
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->wall_image, (x * 64), (y * 64));
+            else if(map[y][x] == 'C')
             {
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->player_image, x * 50, y * 50);
-                game->p_x = x * 50;
-                game->p_y = y * 50;
+                game->collects_n++;
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->coins_image, (x * 64), (y * 64));
             }
-            else if(tab[i] == 'C')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->coins_image, x * 50, y * 50);
-            else if(tab[i] == '0')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->background_image, x * 50, y * 50);
-            else if(tab[i] == 'E')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->exit_image, x * 50, y * 50);
-            else if(tab[i] == 'M')
-                mlx_put_image_to_window(game->mlx, game->mlx_win, images->monster_image, x * 50, y * 50);
+            else if(map[y][x] == 'P')
+            {
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->player_image, (x * 64), (y * 64));
+                game->p_x = x * 64;
+                game->p_y = y * 64;
+            }
+            else if(map[y][x] == 'E')
+            {
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->exit_image, (x * 64), (y * 64));
+                game->exit_x = x * 64;
+                game->exit_y = y * 64;
+            }
+            else if(map[y][x] == 'M')
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->monster_image, (x * 64), (y * 64));
+            else if(map[y][x] == '0')
+                mlx_put_image_to_window(game->mlx, game->mlx_win, images->background_image, (x * 64), (y * 64));
             x++;
-            i++;
         }
-        map = map->next;
         y++;
     }
 }
