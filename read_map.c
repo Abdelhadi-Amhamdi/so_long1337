7f8365978ae3738782b17912cb584f38082d84a7
Abdelhadi Amhamdi <aamhamdi@e1r8p13.1337.ma>
2023-02-04 15:26:13 +0100

    [fix] reading function

--- read_map.c
+++ read_map.c
@@ -6,44 +6,27 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:25:08 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/27 12:01:22 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/02/04 12:04:55 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "gnl/get_next_line.h"
 
-t_list *read_map(char *map_file)
+char **read_map(char *map_file)
 {
-    t_list *map;
+    char *map;
     char *line;
-    t_list *node;
     int fd;
-    int size = 0;
-    char **map_tab;
-    int i = 0;
     
-    line = NULL;
-    map = NULL;
+    map = ft_calloc(1, 1);
     fd = open(map_file, O_RDONLY);
     line = get_next_line(fd);
     while(line)
     {
-        size++;
-        if(!map)
-        {
-            map = ft_lstnew(line);
-            if (!map)
-                return (NULL);
-        }
-        else
-        {
-            node = ft_lstnew(line);
-            if(!node)
-                return (NULL);
-            ft_lstadd_back(&map, node);
-        }
+        map = ft_strjoin(map, line);
+        free(line);
         line = get_next_line(fd);
     }
-    return map;
+    return (ft_split(map, '\n'));
 }
