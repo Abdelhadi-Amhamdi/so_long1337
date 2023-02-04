80e1f90ae54ff080cea65a73e0e8d8813c4927b0
Abdelhadi Amhamdi <aamhamdi@e1r8p13.1337.ma>
2023-02-04 15:26:33 +0100

    [fix] mouves function

--- mouves.c
+++ mouves.c
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 14:57:07 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/27 17:44:47 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:22:52 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,98 +17,117 @@
 
 char get_val(int x, int y, to_long *game)
 {
+    if(x < 0 || y < 0)
+        return 0;
+    return (game->map[y / 64][x / 64]);
+}
 
-    t_list *map;
-    int i = 0;
-
-    map = game->map;
-    while(map)
-    {
-        if(i == (y / 50))
-        {
-            char *data;
-
-            data = (char *)map->content;
-            return (data[(x / 50)]);
-            break;
-        }
-        map = map->next;
-        i++;
-    }
-    return (0);
+void set_val(to_long *game, int x, int y, char c)
+{
+    game->map[y / 64][x / 64] = c;
 }
 
 int  move_player(int keycode, to_long *game)
 {
-    void *background_img;
-    void *player_img;
-    int width = 50;
     char c;
+    t_images images;
+    static int collects;
 
-    background_img = mlx_xpm_file_to_image(game, "images/background.xpm", &width, &width);
-    player_img = mlx_xpm_file_to_image(game, "images/player_right.xpm", &width, &width);
+    ft_init_images(game, &images);
     if(keycode == 124)
     {
-        c = get_val((game->p_x + 50), game->p_y, game);
-        if(c == 'E' || c == 'M')
+        c = get_val((game->p_x + 64), game->p_y, game);
+        if( c == '0' || c == 'C')
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            ft_putendl_fd("u lose !!!", 1);
-            exit (0);
+            if(c == 'C')
+            {
+                collects++;
+                if(collects == game->collects_n)
+                    mlx_put_image_to_window(game->mlx, game->mlx_win, images.open_exit, game->exit_x, game->exit_y);
+                
+            }
+            set_val(game, (game->p_x), (game->p_y), '0');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.background_image, (game->p_x), (game->p_y));
+            game->p_x += 64;
+            set_val(game, (game->p_x), (game->p_y), 'P');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.player_right_image, game->p_x, game->p_y);
         }
-        else if( c != '1')
+        else if(c == 'E' && collects == game->collects_n)
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            game->p_x += 50;
-            mlx_put_image_to_window(game->mlx, game->mlx_win, player_img, game->p_x, game->p_y);
+            printf("you win !!!.\n");
+            exit(0);
         }
     }
     else if(keycode == 123)
     {
-        c = get_val((game->p_x - 50), game->p_y, game);
-        if(c == 'E' || c == 'M')
+        c = get_val((game->p_x - 64), game->p_y, game);
+        if( c == '0' || c == 'C')
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            ft_putendl_fd("u lose !!!", 1);
-            exit (0);
+            if(c == 'C')
+            {
+                collects++;
+                if(collects == game->collects_n)
+                    mlx_put_image_to_window(game->mlx, game->mlx_win, images.open_exit, game->exit_x, game->exit_y);
+                
+            }
+            set_val(game, (game->p_x), (game->p_y), '0');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.background_image, (game->p_x), (game->p_y));
+            game->p_x -= 64;
+            set_val(game, (game->p_x), (game->p_y), 'P');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.player_left_image, game->p_x, game->p_y);
         }
-        else if( c != '1')
+        else if(c == 'E' && collects == game->collects_n)
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            game->p_x -= 50;
-            mlx_put_image_to_window(game->mlx, game->mlx_win, player_img, game->p_x, game->p_y);
+            printf("you win !!!.\n");
+            exit(0);
         }
     }
     else if(keycode == 126)
     {
-        c = get_val((game->p_x), (game->p_y - 50), game);
-        if(c == 'E' || c == 'M')
+        c = get_val((game->p_x), (game->p_y - 64), game);
+        if(c == '0' || c == 'C')
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            ft_putendl_fd("u lose !!!", 1);
-            exit (0);
+            if(c == 'C')
+            {
+                collects++;
+                if(collects == game->collects_n)
+                    mlx_put_image_to_window(game->mlx, game->mlx_win, images.open_exit, game->exit_x, game->exit_y);
+                
+            }
+            set_val(game, (game->p_x), (game->p_y), '0');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.background_image, (game->p_x), (game->p_y));
+            game->p_y -= 64;
+            set_val(game, (game->p_x), (game->p_y), 'P');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.player_image, game->p_x, game->p_y);
         }
-        else if(c != '1')
+        else if(c == 'E' && collects == game->collects_n)
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            game->p_y -= 50;
-            mlx_put_image_to_window(game->mlx, game->mlx_win, player_img, game->p_x, game->p_y);
+            printf("you win !!!.\n");
+            exit(0);
         }
     }
     else if(keycode == 125)
     {
-        c = get_val(game->p_x, (game->p_y + 50), game);
-        if(c == 'E' || c == 'M')
+        c = get_val(game->p_x, (game->p_y + 64), game);
+        if(c == '0' || c == 'C')
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            ft_putendl_fd("u lose !!!", 1);
-            exit (0);
+            if(c == 'C')
+            {
+                collects++;
+                if(collects == game->collects_n)
+                    mlx_put_image_to_window(game->mlx, game->mlx_win, images.open_exit, game->exit_x, game->exit_y);
+                
+            }
+            set_val(game, (game->p_x), (game->p_y), '0');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.background_image, (game->p_x), (game->p_y));
+            game->p_y += 64;
+            set_val(game, (game->p_x), (game->p_y), 'P');
+            mlx_put_image_to_window(game->mlx, game->mlx_win, images.player_image, game->p_x, game->p_y);
         }
-        else if(c != '1')
+        else if(c == 'E' && collects == game->collects_n)
         {
-            mlx_put_image_to_window(game->mlx, game->mlx_win, background_img, (game->p_x), (game->p_y));
-            game->p_y += 50;
-            mlx_put_image_to_window(game->mlx, game->mlx_win, player_img, game->p_x, game->p_y);
+            printf("you win !!!.\n");
+            exit(0);
         }
     }
     return 1;
