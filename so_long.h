f4fb2b42b58a19f8e792dc0a88a6c768ad635277
Abdelhadi Amhamdi <aamhamdi@e1r8p13.1337.ma>
2023-02-04 15:28:20 +0100

    first commmit

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:24:21 by aamhamdi          #+#    #+#             */
-/*   Updated: 2023/01/27 16:40:49 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2023/02/04 15:09:06 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,28 @@ typedef struct so_long
     int heigth;
     int p_x;
     int p_y;
-    t_list *map;
+    int exit_x;
+    int exit_y;
+    int collects_n;
+    char **map;
 } to_long;
 
 
 typedef struct s_images
 {
     void *wall_image;
+    void *player_right_image;
+    void *player_left_image;
     void *player_image;
     void *coins_image;
     void *background_image;
     void *exit_image;
     void *monster_image;
+    void *open_exit;
 } t_images;
 
-t_list *read_map(char *map_file);
-void ft_draw_map(to_long *game, t_images *images, t_list *map);
+char **read_map(char *map_file);
+void ft_draw_map(to_long *game, t_images *images);
 int  move_player(int keycode, to_long *game);
 void ft_init_images(to_long *game, t_images *images);
 char get_val(int x, int y, to_long *game);
